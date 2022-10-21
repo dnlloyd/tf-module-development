46c75aa9d23044b93596ad21efc89a459f27fc54
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-10-21 15:35:02 -0500

    Testing

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_s3_bucket" "selected" {
   bucket = "tf-remote-state-fh-sand"
 }
 
-variable "bucket_name" {}
+# variable "bucket_name" {}
 
 resource "aws_s3_bucket" "bucket" {
   # bucket_prefix = var.bucket_name
