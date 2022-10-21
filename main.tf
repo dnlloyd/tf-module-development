6f92f7ab7cb0a1997319342bce135d6563d12d4c
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-10-21 12:26:13 -0500

    Testing

--- main.tf
+++ main.tf
@@ -20,14 +20,10 @@ terraform {
   }
 }
 
-
 data "aws_s3_bucket" "selected" {
   bucket = "tf-remote-state-fh-sand"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = "test"
-  acl    = "private"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
 }
-
-
