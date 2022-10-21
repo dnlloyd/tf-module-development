4c542c461754c09a4e9c8d1d28a237a1fc04f2f4
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-10-21 15:00:01 -0500

    Testing

--- tests/test-a/main.tf
+++ tests/test-a/main.tf
@@ -20,7 +20,19 @@ terraform {
   }
 }
 
-module "test_a" {
-  source = "../.."
-  # bucket_name = "Bucket-A"
+# module "test_a" {
+#   source = "../.."
+#   # bucket_name = "Bucket-A"
+# }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
